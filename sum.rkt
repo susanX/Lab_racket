2e709bd230e849d9933032a2b5c2fcca895ec506
susanX <susan.gooding@yahoo.co.uk>
2014-11-15 12:18:56 +0000

    Racket
    Sum a list

---
+++ sum.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define(sum list)
+  
+  (if(empty? list)
+     
+     0
+     
+     (+(car list)
+       
+       (sum(cdr list)
+           
+           ))))
+
+"-------------------------------"
+(sum(list 1 2 3 4 5 6 7 8 9 0 ))
